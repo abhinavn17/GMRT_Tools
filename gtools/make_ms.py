5984ff9125bd5d4cb8f10e9e32a73ff26db70703
wonpon <abhinavnarayan7@gmail.com>
2025-02-28 16:59:15 +0530

    Changes to be committed: 	modified:   gtools/make_ms.py

--- gtools/make_ms.py
+++ gtools/make_ms.py
@@ -1,4 +1,4 @@
-from casatasks import importgmrt
+from casatasks import importgmrt, concat, mstransform
 import argparse
 import os
 from .main import run_container
@@ -23,44 +23,110 @@ def main():
 
         run_container('ltamerge', ['-i', ltb_name, '-I', lta_name])
 
-        print('ltamerge done!')
+        lta_merge_size = os.path.getsize('ltamerge_out.lta')
 
-        print('Converting to FITS...')
+        if lta_merge_size < 1000:
+            
+            print('ltamerge failed!')
 
-        run_container('listscan', ['ltamerge_out.lta'])
+            os.remove('ltamerge_out.lta')
 
-        run_container('gvfits', ['ltamerge_out.log'])
+            print('Will run seperate listscan on lta and ltb files...')
 
-        print('FITS conversion done!')
+            run_container('listscan', [lta_name])
 
-        print('Converting to MS...')
+            run_container('gvfits', [lta_name.split('.')[0] + '.log'])
 
-        if args.output is None:
+            os.system('mv TEST.FITS TEST1.FITS')
 
-            output = lta_name.split('.')[0] + '.ms'
+            run_container('listscan', [ltb_name])
+
+            run_container('gvfits', [ltb_name.split('.')[0] + '.log'])
+
+            os.system('mv TEST.FITS TEST2.FITS')
+
+            print('Converting to MS...')
+
+            if args.output is None:
+
+                output = lta_name.split('.')[0] + '.ms'
+            
+            else:
+
+                output = args.output
+
+            if os.path.exists(output):
+
+                os.system('rm -rf ' + output + '*')
+
+            output1 = output + '.1'
+            output2 = output + '.2'
+
+            importgmrt(vis=output1, fitsfile='TEST1.FITS')
+            importgmrt(vis=output2, fitsfile='TEST2.FITS')
+
+            print('MS conversion done!')
+
+            print('Combining MS files...')
+
+            output_combined = output + '.combined'
+
+            concat(vis=[output1, output2], concatvis=output_combined, timesort=True)
+
+            mstransform(vis=output_combined, outputvis=output, datacolumn='DATA', combinespws=True)
+
+            print('Deleting temporary files...')
+
+            os.remove('TEST1.FITS')
+            os.remove('TEST2.FITS')
+
+            os.system('rm -rf ' + output1 + '*')
+            os.system('rm -rf ' + output2 + '*')
+
+            os.system('rm -rf ' + output_combined + '*')
+            os.system('rm -rf ' + output1 + '*')
+            os.system('rm -rf ' + output2 + '*')
 
         else:
 
-            output = args.output
+            print('ltamerge done!')
 
-        if os.path.exists(output):
+            print('Converting to FITS...')
 
-            os.system('rm -rf ' + output + '*')
+            run_container('listscan', ['ltamerge_out.lta'])
 
-        importgmrt(vis=output, fitsfile='TEST.FITS')
+            run_container('gvfits', ['ltamerge_out.log'])
 
-        print('MS conversion done!')
+            print('FITS conversion done!')
 
-        print('Deleting temporary files...')
+            print('Converting to MS...')
 
-        os.remove('ltamerge_out.lta')
-        os.remove('TEST.FITS')
+            if args.output is None:
+
+                output = lta_name.split('.')[0] + '.ms'
+
+            else:
+
+                output = args.output
+
+            if os.path.exists(output):
+
+                os.system('rm -rf ' + output + '*')
+
+            importgmrt(vis=output, fitsfile='TEST.FITS')
+
+            print('MS conversion done!')
+
+            print('Deleting temporary files...')
+
+            os.remove('ltamerge_out.lta')
+            os.remove('TEST.FITS')
 
     elif len(args.input) == 1:
 
         file = args.input[0]
 
-        if file.split('.')[-1] == 'lta' or file.split('.')[-2] == 'lta' or file.split('.')[-1] == 'LTA' or file.split('.')[-2] == 'LTA':
+        if file.split('.')[-1] == 'lta' or file.split('.')[-2] == 'lta' or file.split('.')[-1] == 'LTA' or file.split('.')[-2] == 'LTA' or file.split('.')[-1] == 'ltb' or file.split('.')[-2] == 'ltb' or file.split('.')[-1] == 'LTB' or file.split('.')[-2] == 'LTB':
             
             filename = file.split('.')[0]
 
